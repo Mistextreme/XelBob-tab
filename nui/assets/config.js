cb72f657c174a0b872441ad34991fd1bab057921
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2026-02-23 09:10:39 +0000

    Upload: config.js

---
+++ nui/assets/config.js
@@ -0,0 +1,56 @@
+/* const config = {
+    sites : [
+        { name: 'Xelyos', icon: 'img/xelyos.png', href: 'xelyosapp', url: "https://xelyos.fr" },
+        { name: 'Mail RP', icon: 'img/mailrp.png', href: 'mailrpapp', url: "https://mail-rp.com/xelyos" },
+        { name: 'Top Serveur', icon: 'img/top-serveur.png', href: 'topserveurapp', url: "https://top-serveurs.net" },
+        { name: 'COP app', icon: 'img/lspd.png', href: 'copapp', url: "https://intranet-lspd.xelyos.fr" },
+        { name: 'SECOURS app', icon: 'img/samu.png', href: 'secoursapp', url: "https://intranet-ems.xelyos.fr" },
+        { name: 'Calculator', icon: 'img/calculator.png', href: 'calculatorapp' },
+        { name: 'Note', icon: 'img/note.png', href: 'noteapp' }, 
+    ]
+};
+
+
+// Attribuer des sites en fonction du jobs
+const jobSites = [
+    {
+        job_name: "police",
+        sites: [
+            { name: 'COP app', icon: 'img/lspd.png', href: 'copapp', url: "https://intranet-lspd.xelyos.fr" },
+        ]
+    },
+    {
+        job_name: "ambulance",
+        sites: [
+            { name: 'SECOURS app', icon: 'img/samu.png', href: 'secoursapp', url: "https://intranet-ems.xelyos.fr" },
+        ]
+    }
+]; */
+
+const config = {
+    sites: [
+        { name: 'Xelyos', icon: 'img/xelyos.png', href: 'xelyosapp', url: "https://xelyos.fr" },
+        { name: 'Mail RP', icon: 'img/mailrp.png', href: 'mailrpapp', url: "https://mail-rp.com/xelyos" },
+        { name: 'Top Serveur', icon: 'img/top-serveur.png', href: 'topserveurapp', url: "https://top-serveurs.net" },
+        { name: 'COP app', icon: 'img/lspd.png', href: 'copapp', url: "https://intranet-lspd.xelyos.fr" },
+        { name: 'SECOURS app', icon: 'img/samu.png', href: 'secoursapp', url: "https://intranet-ems.xelyos.fr" },
+        { name: 'Calculator', icon: 'img/calculator.png', href: 'calculatorapp' },
+        { name: 'Note', icon: 'img/note.png', href: 'noteapp' },
+    ]
+};
+
+// Attribuer des sites en fonction des jobs
+const jobSites = [
+    {
+        job_name: "police",
+        sites: [
+            { name: 'COP app', icon: 'img/lspd.png', href: 'copapp', url: "https://intranet-lspd.xelyos.fr" },
+        ]
+    },
+    {
+        job_name: "ambulance",
+        sites: [
+            { name: 'SECOURS app', icon: 'img/samu.png', href: 'secoursapp', url: "https://intranet-ems.xelyos.fr" },
+        ]
+    }
+];
